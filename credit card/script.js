983d7052f8b060ccc171a744fe68ded3522d7e0d
Harenabs21 <123376410+Harenabs21@users.noreply.github.com>
2023-03-04 10:22:14 +0300

    Add files via upload
    a cool credit  card

---
+++ credit card/script.js
@@ -0,0 +1,8 @@
+/*function endAnim(){
+    const loader = document.querySelector('#container');
+    const progressBar = document.querySelector('#Loader');
+    progressBar.addEventListener('transitionend', function() {
+        loader.style.display = 'none';
+        document.body.classList.remove('hidden');
+      });
+}**/
